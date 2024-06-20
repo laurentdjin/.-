90f346ac66dba66d9c2c97c44a30649504d2f708
JIN Laurent <laurentjin@live.fr>
2024-06-20 16:21:25 +0200

    modif fichier fib et pal .h

--- include/fibonacci.h
+++ include/fibonacci.h
@@ -2,10 +2,12 @@
 #define FIBONACCI_H
 
 /**
-* @file fibonacci.h
-* @brief Déclaration de la fonction de calcul de Fibonacci
-* @param a First integer.
-*/
+ * @file fibonacci.h
+ *
+ * @brief Déclaration de la fonction de calcul de Fibonacci
+ *
+ * @param a First integer.
+ */
 
 int fibonacci(int a);
 
--- include/palindrome.h
+++ include/palindrome.h
@@ -4,10 +4,13 @@
 #include <string>
 
 /**
-* @file palindrome.h
-* @brief Déclaration de la fonction de vérification de palindrome
-* @param a string.
-*/
+ * @file palindrome.h
+ *
+ * @brief Déclaration de la fonction de vérification de palindrome
+ *
+ * @param a string qui retourne un boleen.
+ *
+ */
 
 
 bool isPalindrome(const std::string& str);
