f65b8cc2485e12bf74856f9aa3b98264c4e1b352
JIN Laurent <laurentjin@live.fr>
2024-06-20 15:56:38 +0200

    modif dossier tests et Doxifile

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/laurentjin/CMAKE/ex6/.-
+# Build directory: /home/laurentjin/CMAKE/ex6/.-/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/laurentjin/CMAKE/ex6/.-/tests
+# Build directory: /home/laurentjin/CMAKE/ex6/.-/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
